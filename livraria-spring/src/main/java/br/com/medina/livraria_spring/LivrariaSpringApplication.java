42adccdf1e259de1b8fae94d53c187db4fde9469
michaelmedina10 <michael.cunha97@hotmail.com>
2024-07-18 08:05:27 -0300

    First Commit

---
+++ livraria-spring/src/main/java/br/com/medina/livraria_spring/LivrariaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.medina.livraria_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaSpringApplication.class, args);
+	}
+
+}
---
+++ livraria-spring/src/test/java/br/com/medina/livraria_spring/LivrariaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.medina.livraria_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
